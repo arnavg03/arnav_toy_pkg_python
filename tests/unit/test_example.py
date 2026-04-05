3e3796be6875208b15d364bf8d8a2906073a94a0
Arnav Gupta <arnav.gu03@gmail.com>
2026-04-04 19:11:16 -0700

    fix: update unit tests for quick_sort

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -7,14 +7,17 @@ is up to you. This test example provides a single test for the example.py
 module.
 """
 
-from arnav_toy_pkg.example import add_numbers
+from arnav_toy_pkg.example import quick_sort
 
-def test_add_numbers():
-    """
-    Test that add_numbers works as expected.
+def test_quick_sort_standard():
+    """Test standard list sorting."""
+    my_list = [34, 7, 23, 32, 5, 62, 32]
+    assert quick_sort(my_list) == [5, 7, 23, 32, 32, 34, 62]
 
-    A single line docstring for tests is generally sufficient.
-    """
-    out = add_numbers(1, 2)
-    expected_out = 3
-    assert  out == expected_out, f"Expected {expected_out} but got {out}"
+def test_quick_sort_empty():
+    """Test empty list."""
+    assert quick_sort([]) == []
+
+def test_quick_sort_single():
+    """Test list with one element."""
+    assert quick_sort([1]) == [1]
